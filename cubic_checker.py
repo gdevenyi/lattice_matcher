4d264462b79d6b71b0027dfec9909ecc480ff4bb
Stephen Smith <stephen.j.smith.1991@gmail.com>
2013-10-18 10:29:48 -0400

    Add numpy.genfromtxt() input file reading. Output file is tab delimited .txt file.

--- cubic_checker.py
+++ cubic_checker.py
@@ -8,40 +8,28 @@
 import numpy
 
 # film file must have ONLY cubic symmetries for this program
-film_file = open("cubic.txt", "r")
-substrate_file = open("hexagonal.txt", "r")
-matches_file = open("cubic_matches_h.csv", "w")
+film_file = numpy.genfromtxt("cubic.txt", skip_header=1, delimiter="\t", dtype=None)
+substrate_file = numpy.genfromtxt("hexagonal.txt", skip_header=1, delimiter="\t", dtype=None)
+matches_file = open("cubic_matches_h.txt", "w")
 
 tolerance = 0.08 #tolerance of 8% for mismatch
 
 def search_film_file(film_file, sub_comp, sub_sym, sub_a, sub_c):
     # searches the film file for matches with the substrate 
-    for line in film_file:
-        column = line.split("\t") #splits the line into a list using tabs (\t) as the delimiter
-        film_comp = str(column[0])
-        film_sym = str(column[1])
-        film_a = float(column[2])
-        film_c = float(column[3])
+    for i, l in enumerate(film_file):
         if sub_sym == "C":
-            cubic_sub(film_comp, film_sym, sub_comp, sub_sym, sub_a, sub_c, film_a, film_c)
+            cubic_sub(film_file[i][0], film_file[i][1], sub_comp, sub_sym, sub_a, sub_c, film_file[i][2], film_file[i][3])
         elif sub_sym == "T":
-            tetragonal_sub(film_comp, film_sym, sub_comp, sub_sym, sub_a, sub_c, film_a, film_c)
+            tetragonal_sub(film_file[i][0], film_file[i][1], sub_comp, sub_sym, sub_a, sub_c, film_file[i][2], film_file[i][3])
         elif sub_sym == "H":
-            hexagonal_sub(film_comp, film_sym, sub_comp, sub_sym, sub_a, sub_c, film_a, film_c)
-    film_file.seek(0) # .seek(0) returns the pointer to the top of the file so Python may scan through the file again
+            hexagonal_sub(film_file[i][0], film_file[i][1], sub_comp, sub_sym, sub_a, sub_c, film_file[i][2], film_file[i][3])
 
 def lattice_check(film_file, substrate_file):
     # Reads the material in the substrate list and searches the entire film file for matches
     # Read the next substrate material and search the film file again
-    matches_file.write("Film,Symmetry,Substrate,Symmetry,Mismatch(%),Rounded Ratio,Original Ratio,C Mismatch(%),C Rounded Ratio,C Original Ratio" + "\n")
-    for line in substrate_file:
-        column = line.split("\t")
-        sub_comp = str(column[0])
-        sub_sym = str(column[1])
-        sub_a = float(column[2])
-        sub_c = float(column[3])
-        search_film_file(film_file, sub_comp, sub_sym, sub_a, sub_c)
-    substrate_file.seek(0)
+    matches_file.write("Film\tSymmetry\tSubstrate\tSymmetry\tMismatch(%)\tRounded Ratio\tOriginal Ratio\tC Mismatch(%)\tC Rounded Ratio\tC Original Ratio\n")
+    for i, l in enumerate(substrate_file):
+        search_film_file(film_file, substrate_file[i][0], substrate_file[i][1], substrate_file[i][2], substrate_file[i][3])
 
 def ratio_cal(original_ratio):
     #rounds the original ratio
@@ -57,7 +45,7 @@ def cubic_sub(film_comp, film_sym, sub_comp, sub_sym, sub_a, sub_c, film_a, film
     ratio = ratio_cal(original_ratio)
     mismatch = ((sub_a - (ratio*film_a)) / sub_a)
     if abs(mismatch) < tolerance:
-        matches_file.write(film_comp + "," + film_sym + "," + sub_comp + "," + sub_sym + "," + str(mismatch) + "," + str(ratio) + "," + str(original_ratio) + "\n")
+        matches_file.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(film_comp, film_sym, sub_comp, sub_sym, mismatch, ratio, original_ratio))
 
 def tetragonal_sub(film_comp, film_sym, sub_comp, sub_sym, sub_a, sub_c, film_a, film_c):
     # called if the substrate has tetragonal symmetry
@@ -68,8 +56,9 @@ def tetragonal_sub(film_comp, film_sym, sub_comp, sub_sym, sub_a, sub_c, film_a,
     mismatch1 = ((sub_a - (ratio1*film_a)) / sub_a)
     mismatch2 = ((sub_c - (ratio2*numpy.sqrt(2.0)*film_a)) / sub_c) 
     if abs(mismatch1) < tolerance:
-        matches_file.write(film_comp + "," + film_sym + "," + sub_comp + "," + sub_sym + "," + str(mismatch1) + "," + str(ratio1) + "," + str(original_ratio1) + "\n")
+        matches_file.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(film_comp, film_sym, sub_comp, sub_sym, mismatch1, ratio1, original_ratio1))
     if ratio_check(sub_c, sub_a) < tolerance and abs(mismatch1) < tolerance: #check if ratio of sub_c/sub_a is close to sqrt(2.0). If not then cubic (110) will definitely not match
+        matches_file.write("{}\t{} (110)\t{}\t{} (a-plane)\t{}\t{}\t{}\t{}\t{}\t{}\n".format(film_comp, film_sym, sub_comp, sub_sym, mismatch1, ratio1, original_ratio1, mismatch2, ratio2, original_ratio2))
         matches_file.write(film_comp + "," + film_sym + " (110)," + sub_comp + "," + sub_sym + " (a-plane)," + str(mismatch1) + "," + str(ratio1) + "," + str(original_ratio1) + "," + str(mismatch2) + "," + str(ratio2) + "," + str(original_ratio2)+ "\n")
 
 def hexagonal_sub(film_comp, film_sym, sub_comp, sub_sym, sub_a, sub_c, film_a, film_c):
@@ -88,11 +77,11 @@ def hexagonal_sub(film_comp, film_sym, sub_comp, sub_sym, sub_a, sub_c, film_a, 
     mismatch4 = ((numpy.sqrt((sub_c**2)+(3*(sub_a**2))) - (ratio3*film_a*numpy.sqrt(2.0))) / numpy.sqrt((sub_c**2)+(3*(sub_a**2))))
     temp_c = numpy.sqrt((sub_c**2)+(3*(sub_a**2))) #side length for camparison of r-plane hex side lengths
     if abs(mismatch1) < tolerance:
-        matches_file.write(film_comp + "," + film_sym + " (111)," + sub_comp + "," + sub_sym + "," + str(mismatch1) + "," + str(ratio1) + "," + str(original_ratio1) + "\n")
+        matches_file.write("{}\t{} (111)\t{}\t{}\t{}\t{}\t{}\n".format(film_comp, film_sym, sub_comp, sub_sym, mismatch1, ratio1, original_ratio1))
     if abs(mismatch2) < tolerance and ratio_check(sub_c, sub_a) < tolerance:
-        matches_file.write(film_comp + "," + film_sym + " (110)," + sub_comp + "," + sub_sym + " (a-plane)," + str(mismatch2) + "," + str(ratio2) + "," + str(original_ratio2) + "," + str(mismatch3) + "," + str(ratio3) + "," + str(original_ratio3) + "\n")
+        matches_file.write("{}\t{} (110)\t{}\t{} (a-plane)\t{}\t{}\t{}\t{}\t{}\t{}\n".format(film_comp, film_sym, sub_comp, sub_sym, mismatch2, ratio2, original_ratio2, mismatch3, ratio3, original_ratio3))
     if abs(mismatch2) < tolerance and ratio_check(temp_c, sub_a) < tolerance:
-        matches_file.write(film_comp + "," + film_sym + " (110)," + sub_comp + "," + sub_sym + " (r-plane)," + str(mismatch2) + "," + str(ratio2) + "," + str(original_ratio2) + "," + str(mismatch4) + "," + str(ratio4) + "," + str(original_ratio4) + "\n")
+        matches_file.write("{}\t{} (110)\t{}\t{} (r-plane)\t{}\t{}\t{}\t{}\t{}\t{}\n".format(film_comp, film_sym, sub_comp, sub_sym, mismatch2, ratio2, original_ratio2, mismatch4, ratio4, original_ratio4))
 
 def ratio_check(c_value, a_value):
     #check to see if ratio of a and c values for hexagonal and tetragonal symmetries is close to sqrt(2)
@@ -101,6 +90,4 @@ def ratio_check(c_value, a_value):
     
 lattice_check(film_file, substrate_file)
 
-film_file.close()
-substrate_file.close()
 matches_file.close()
